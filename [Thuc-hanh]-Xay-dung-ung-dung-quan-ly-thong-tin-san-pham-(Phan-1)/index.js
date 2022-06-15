125ec29ec747e53acfaada4168d4689c7624de91
superman <duonga2qp@gmail.com>
2022-06-15 14:21:49 +0700

    nopbai

--- [Thuc-hanh]-Xay-dung-ung-dung-quan-ly-thong-tin-san-pham-(Phan-1)/index.js
+++ [Thuc-hanh]-Xay-dung-ung-dung-quan-ly-thong-tin-san-pham-(Phan-1)/index.js
@@ -1,57 +1,71 @@
 const mysql = require('mysql');
 const http = require('http');
-const buffer = require("buffer");
+const qs = require('qs')
+const util = require('util');
+
 
 const connection = mysql.createConnection({
-    user:'root',
-    password:'password',
-    host:'localhost',
-    port:'3305',
-    database:'manager',
-    charset:'utf8_general_ci'
+    user: 'root',
+    password: 'password',
+    host: 'localhost',
+    port: '3305',
+    database: 'manager',
+    charset: 'utf8_general_ci'
 });
 
-connection.connect(err=>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log('connect success')
+// convert query to promise
+const query = util.promisify(connection.query).bind(connection);
+
+
+// connection.connect(err=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log('connect success');
+//         const sqlCreateTable = "create table if not exists products (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,name varchar(30) not null, price INT NOT NULL)";
+//         connection.query(sqlCreateTable,(err)=>{
+//             if(err){
+//                 console.log(err.message);
+//             }else{
+//                 console.log('create table success')
+//             }
+//         });
+//     }
+// })
+
+
+const server = http.createServer(async (req, res) => {
+    try {
+        const sqlCreateTable = "create table if not exists products (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,name varchar(30) not null, price INT NOT NULL)";
+        let results = await query(sqlCreateTable);
+        console.log('them bang thanh cong')
+    } catch (err) {
+        console.log(err)
     }
-})
 
-
-const sqlCreateTable = "create table if not exists products (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,name varchar(30) not null, price INT NOT NULL)";
-connection.query(sqlCreateTable,(err)=>{
-    if(err){
-        console.log(err.message);
-    }else{
-        console.log('create table success')
-    }
-});
-
-const server = http.createServer(async(req, res) => {
-    try{
-        if(req.url==='/product/create'&& req.method ==='POST'){
-            const buffers = [];
-            for await (const chunk of req){
-                buffers.push(chunk)
-            }
-            const data = Buffer.concat(buffers).toString()
-            console.log(data +'123')
-            const product = JSON.parse(data);
-            const price = parseInt(product.price)
-            console.log('product'+ product);
-            const sqlCreate = `INSERT INTO products(name, price) VALUES ('${product.name}', ${price});`;
-            connection.query(sqlCreate, (err, results, fields) => {
-                if (err) throw err;
-                res.end(JSON.stringify(product))
+    try {
+        if (req.url === '/product/create' && req.method === 'POST') {
+            let data = '';
+            console.log('123')
+            req.on('data', chunk => {
+                data += chunk;
+                console.log(data+'123');
+            })
+            let product = ''
+            req.on('end', async() => {
+                product = qs.parse(data)
+                console.log(product);
+                const sqlCreate = `INSERT INTO products(name, price) VALUES ('${product.name}', ${product.price});`;
+                let result = await query(sqlCreate);
+                console.log(`them ${product.name} thanh cong`)
             });
+            res.end('them thanh cong')
         }
-    }catch (e){
+    } catch (e) {
         return res.end(e.message)
     }
 })
 
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log('server run at 3000')
 })
